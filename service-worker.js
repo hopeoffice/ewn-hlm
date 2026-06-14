d34f223486660a645ea88a04dc168914099d9f8f
hope-office <dannniefx55@gmail.com>
2026-06-14 22:41:46 +0300

    Update CACHE_NAME and asset paths in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,12 @@
-const CACHE_NAME = 'ewn-hlm-v5';
+const CACHE_NAME = 'ewn-hlm-v4';
 const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './main.js',
-  './products.json',
-  './manifest.json',
-  './version.json',
-  './1.png'
+  '/',
+  '/index.html',
+  '/style.css',
+  '/main.js',
+  '/products.json',
+  '/manifest.json',
+  '/1.png'
 ];
 
 // Install: cache core assets
